31562a83abdc4e0cf30adcd357911385a235210a
Đăng Võ Nguyễn <vndang96@gmail.com>
2026-06-19 16:45:48 +0700

    fix: tự động cắt bỏ chữ Dater (ảo giác từ Date of expiry) trong phần địa chỉ

--- wizard/main.py
+++ wizard/main.py
@@ -621,7 +621,7 @@ def parse_ocr_text(text):
                                 
                             # CẮT BỎ CÁC TỪ TIẾNG ANH ẢO GIÁC DO OCR NHẬN DIỆN MỜ VÀ CÁC NHÃN
                             clean_line = re.sub(
-                                r'(?i)\b(substates|date|place\s*of\s*res[a-z]*|place\s*ofresic|i\s*place|pplace|ppace|place|'
+                                r'(?i)\b(substates|date|dater|place\s*of\s*res[a-z]*|place\s*ofresic|i\s*place|pplace|ppace|place|'
                                 r'date\s*of\s*issue|ddate|ddate\s*issue|dddate|ddate\s*issue|date\s*issue|issue|'
                                 r'indent|vi[eê][nǹ]|nam\s+linh|'
                                 r'place of residence|place of origin|place oforging|transervating|daleoroxic|'
@@ -2178,7 +2178,7 @@ def clean_address_string(addr):
                         r'gi[oớ]i\s*t[ií]nh|qu[oố]c\s*t[iị]ch|sex|nationality|'
                         r'qu[eê]\s*qu[aá]n|khai\s*sinh|birth|data\s*ofespry)', '', clean_line)
     clean_line = re.sub(r'(?<!\d)\d{2}/\d{2}/\d{4}(?!\d)', '', clean_line)
-    clean_line = re.sub(r'(?i)\b(place\s*of\s*res[a-z]*|place\s*ofresic|i\s*place|pplace|ppace|place|'
+    clean_line = re.sub(r'(?i)\b(dater|place\s*of\s*res[a-z]*|place\s*ofresic|i\s*place|pplace|ppace|place|'
                         r'date\s*of\s*issue|ddate|ddate\s*issue|dddate|ddate\s*issue|date\s*issue|issue|'
                         r'indent|vi[eê][nǹ]|nam\s+linh|'
                         r'place of residence|place of origin|place oforging|transervating|daleoroxic|'
